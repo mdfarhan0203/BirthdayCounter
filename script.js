0e1240e569813629bd800245f56316801a952d9f
mdfarhan0203 <md.farhan0203@gmail.com>
2023-03-02 11:17:42 +0530

    changes year hardcode to dynamically select and month feature modified

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 
+let years
+let remindMonth
+
+
 // when timer is goes 00 to all then bDay wish show
 function wishHappyBirthday(){
-    document.body.style.backgroundImage ="url('./images/BirthdayWish.gif')";
-    document.querySelector('h1').innerHTML = 'Happy Birth Day Md Farhan'
-    document.querySelector('h1').style.color='#E1EEDD'
+    setTimeout(() => {
+        document.body.style.backgroundImage ="url('./images/BirthdayWish.gif')";
+        document.querySelector('h1').innerHTML = 'Happy Birth Day Md Farhan'
+        document.querySelector('h1').style.color='#BAD7E9'
+      }, 1000)
+   
     }
 // time Format When time is goes Less then 10 it will add 0 front of it
 function timeFormat(time) {
@@ -12,30 +19,37 @@ function timeFormat(time) {
 
 // Bday Reminder
 function birthDayReminder() {
-    let birthdayDate = Date.parse('March 02 2023');
-    let currentDate = Date.parse(new Date())
+    let date=new Date()
+    let CurrentMonths = date.getMonth()+1
+    let FixedMonth=3
+    if(CurrentMonths<3)
+        remindMonth=FixedMonth-CurrentMonths
+    else
+        remindMonth=12-(CurrentMonths-FixedMonth)
+    
+    years=date.getFullYear()+1
+    let birthdayDate = Date.parse(`March 02 ${years}`);
+    let currentDate = Date.parse(date)
     let totalSeconds = (birthdayDate - currentDate) / 1000
     let days = Math.floor(totalSeconds / 3600 / 24)
     let hours = Math.floor((totalSeconds / 3600) % 24)
     let minutes = Math.floor(totalSeconds / 60) % 60;
     let seconds = Math.floor(totalSeconds) % 60
-    let months = Math.floor(days / 30) % 12
 
-    // let days=0,hours=0,minutes=0,seconds=0,months=0
+    // let days=0,hours=0,minutes=0,seconds=0
     //birthday Wish
     if (seconds === 0 &&
         minutes === 0 &&
         hours === 0 &&
-        days === 0 &&
-        months === 0) {
-            wishHappyBirthday()
+        days === 0 ) {
+            wishHappyBirthday();
     }
     else {
         document.querySelector('.day').innerHTML = timeFormat(days)
         document.querySelector('.hrs').innerHTML = timeFormat(hours)
         document.querySelector('.min').innerHTML = timeFormat(minutes)
         document.querySelector('.sec').innerHTML = timeFormat(seconds)
-        document.querySelector('.month').innerHTML = timeFormat(months)
+        document.querySelector('.month').innerHTML = timeFormat(remindMonth)
     }
 }
 
